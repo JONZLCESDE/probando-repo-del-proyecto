8a25f4c2121e2f78f07eac5c90a8655f4d875222
Juan Pablo Castillo <jcastillo@cesde.net>
2024-11-19 08:59:38 -0500

    se agregan los cards css

--- proyecto integrador v2/limpieza/prueba2/servicios.js
+++ proyecto integrador v2/limpieza/prueba2/servicios.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+//document.addEventListener("DOMContentLoaded", function() {
     // Ejemplo de funcionalidad: mostrar una alerta al cargar la página de servicios
-    alert("Bienvenido a la página de Servicios");
+   // alert("Bienvenido a la página de Servicios");
 
     // Funcionalidad adicional: mostrar detalles al hacer clic en un servicio específico
-    const servicioElements = document.querySelectorAll(".servicio");
+    //const servicioElements = document.querySelectorAll(".servicio");
 
-    servicioElements.forEach(servicio => {
-        servicio.addEventListener("click", function() {
-            alert(`Has seleccionado el servicio: ${servicio.textContent}`);
-        });
-    });
-});
+    //servicioElements.forEach(servicio => {
+       // servicio.addEventListener("click", function() {
+        //    alert(`Has seleccionado el servicio: ${servicio.textContent}`);
+        //});
+    //});
+//});
